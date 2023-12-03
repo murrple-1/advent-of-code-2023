e654b6f859fbc6d6826213f868d4044c1aa56f6c
Murray Christopherson <murraychristopherson+github@gmail.com>
2023-12-03 05:47:41 +0000

    starting (at a bad time)

---
+++ 01/main_1.py
@@ -0,0 +1,10 @@
+import re
+
+def main():
+    with open("input.txt", "r") as f:
+        sum_ = 0
+        for line in (l.rstrip("\n") for l in f.readlines()):
+            pass
+
+if __name__ == "__main__":
+    main()
